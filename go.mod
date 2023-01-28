244a05aec5be6845c9090b3b22c1dcbe46c52a38
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2023-01-28 17:41:42 -0500

    test

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/AlexBrochu/protobuf-lib-demo/tree/master/proto-lib
-
-go 1.19
--- golang-demo/src/main.go
+++ golang-demo/src/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	// "corpus"
 	"fmt"
-	"github.com/AlexBrochu/protobuf-lib-demo/tree/master/proto-lib"
-	"github.com/spf13/cobra"
 )
 
 func main() {
 	fmt.Println("hello world")
-	
 }
 
 //https://developers.google.com/protocol-buffers/docs/gotutorial
