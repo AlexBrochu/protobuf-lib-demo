0fad507d599814fa23e3e2197161cdb0a72d0989
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2023-01-28 17:24:40 -0500

    test

---
+++ golang-demo/README.md
@@ -0,0 +1,1 @@
+protoc -I. -Ivendors --go_out=vendors proto/test.proto
--- golang-demo/src/main.go
+++ golang-demo/src/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AlexBrochu/protobuf-lib-demo/tree/master/proto-lib"
+	"github.com/spf13/cobra"
+)
 
 func main() {
 	fmt.Println("hello world")
+	
 }
 
 //https://developers.google.com/protocol-buffers/docs/gotutorial
